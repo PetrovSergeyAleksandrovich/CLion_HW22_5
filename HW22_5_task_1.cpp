d57486f5b332f4bfb10d8a031e66e4348d46ec08
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-01 11:25:46 +0300

    task_1 done, text.txt added

--- HW22_5_task_1.cpp
+++ HW22_5_task_1.cpp
@@ -7,18 +7,19 @@ int main() {
     std::string output, user_input;
     int counter = 0;
 
-    std::cout << "Type number like 'one, two, three... ten' to search in file: ";
+    std::cout << "Type number like 'one, two, three... ten' to search in file\n: ";
     std::cin >> user_input;
 
     my_file.open("C:\\Users\\Poizone\\CLion_HW22_5\\text.txt"); /* set your path on your machine*/
-
     while(!my_file.eof())
     {
         my_file >> output;
         if(output == user_input) counter++;
     }
-    my_file.close();
 
+    my_file.close();
     std::cout << user_input << " - found " << counter << " times."<< std::endl;
     return 0;
 }
+
+
