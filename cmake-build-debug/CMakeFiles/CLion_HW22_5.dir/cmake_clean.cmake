31dbc6ff0eb3488114e1ba1c275c7975abd9a994
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-01 10:56:40 +0300

    init commit

---
+++ cmake-build-debug/CMakeFiles/CLion_HW22_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLion_HW22_5.exe"
+  "CLion_HW22_5.exe.manifest"
+  "CLion_HW22_5.lib"
+  "CLion_HW22_5.pdb"
+  "CMakeFiles/CLion_HW22_5.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_HW22_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Init commit!" << std::endl;
+    return 0;
+}
