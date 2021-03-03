934c02b7364b5b13f1bb7b89e2431ed4765159b5
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-03 16:34:20 +0300

    main.cpp tested for PNG. correct

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main() {
-
+int main()
+{
     std::ifstream my_file;
-    std::string output, user_input;
-    int counter = 0;
 
-    std::cout << "Type number like 'one, two, three... ten' to search in file: ";
-    std::cin >> user_input;
-
-    my_file.open("C:\\Users\\Poizone\\CLion_HW22_5\\text.txt"); /* set your path on your machine*/
-
-    while(!my_file.eof())
-    {
-        my_file >> output;
-        if(output == user_input) counter++;
-    }
+    //OPTION 1
+    /* set your path on your machine*/
+    char buffer[5];
+    my_file.open("C:\\Users\\Poizone\\CLion_HW22_5\\picture.png", std::ios::binary);
+    my_file.read(buffer, sizeof(buffer)-1);
+    buffer[0] == -119 && buffer[1] == 'P' && buffer[2] == 'N' && buffer[3] == 'G' ?
+    std::cout << "YES\n" : std::cout << "NO\n";
     my_file.close();
 
-    std::cout << user_input << " - found " << counter << " times."<< std::endl;
+    // test output
+    buffer[4] = 0;
+    std::cout << buffer << std::endl << &buffer << std::endl << buffer[0] << std::endl << buffer[3] ;
+
     return 0;
 }
